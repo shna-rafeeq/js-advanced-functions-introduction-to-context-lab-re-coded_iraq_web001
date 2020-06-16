509a36c4f1870ad32d06d4312ccb1561bcb16dff
shna rafeeq <shnar.majeed@gmail.com>
2020-06-16 06:33:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+ let testEmployee = createEmployeeRecord(["Gray", "Worm", "Security", 1])
+  let twoRows = [
+        ["moe", "sizlak", "barkeep", 2],
+        ["bartholomew", "simpson", "scamp", 3]
+      ]
+ let employeeRecords = createEmployeeRecords(twoRows)
+ 
+function createEmployeeRecord(array){
+    let obj = {};
+     
+  for(let i =0;i<array.length;i++){
+       
+    if(array[i] === "Gray"){
+     obj.firstName = "Gray"
+    }
+     else if(array[i] === "Worm"){
+     obj.familyName = "Worm"
+    }
+    else if(array[i] === "Security"){
+     obj.title = "Security"
+    }
+      else if(array[i] === 1){
+     obj.payPerHour = 1;
+    }
+  
+  }
+  obj.timeInEvents = [];
+  obj.timeOutEvents = [];
+   return obj;
+   
+}
+
+function createEmployeeRecords(array){
+   
+   let employeeRecords = [...twoRows];
+   
+   employeeRecords.map((e)=>{
+   e.map(item =>{
+     console.log("hgkdcukhsjdv")
+   })
+      
+   });
+    return employeeRecords
+}
